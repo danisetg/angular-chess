c28ae7681a79258ff77d9994f357b6fdf2fbfc61
daniveloper <develored@gmail.com>
2019-01-19 18:33:18 -0500

    added pawn movement rules

--- src/app/shared/services/movement-rules/movement-rules.service.ts
+++ src/app/shared/services/movement-rules/movement-rules.service.ts
@@ -14,6 +14,8 @@ export class MovementRulesService {
     switch (piece.name) {
       case 'pawn':
         return this.isAvalidPawnMove(piece, destinationRow, destinationColumn, position);
+      case 'knight':
+        return this.isAvalidKnightMove(piece, destinationRow, destinationColumn, position);
     }
     return false;
   }
@@ -25,7 +27,7 @@ export class MovementRulesService {
     if (destinationRow - piece.row === validDifference) {
       if (destinationColumn === piece.column && !position[destinationRow][destinationColumn]) {
         return true;
-      } else if (Math.abs(this.columns.indexOf(destinationColumn) - this.columns.indexOf(piece.column)) === 1) {
+      } else if (Math.abs(this.columnsDifference(piece.column, destinationColumn)) === 1) {
         if (position[destinationRow][destinationColumn] && position[destinationRow][destinationColumn].color !== piece.color) {
           return true;
         }
@@ -39,5 +41,20 @@ export class MovementRulesService {
       return false;
     }
     return false;
+  }
+  private isAvalidKnightMove (piece: Piece, destinationRow: number, destinationColumn: string, position: any[]): boolean {
+    if (position[destinationRow][destinationColumn] && position[destinationRow][destinationColumn].color === piece.color) {
+      return false;
+    } else {
+      if (Math.abs(piece.row - destinationRow) === 1 && this.columnsDifference(piece.column, destinationColumn) === 2) {
+        return true;
+      } else if (Math.abs(piece.row - destinationRow) === 2 && this.columnsDifference(piece.column, destinationColumn) === 1) {
+        return true;
+      }
+      return false;
+    }
+  }
+  columnsDifference(col1: string, col2: string): number {
+    return Math.abs(this.columns.indexOf(col1) - this.columns.indexOf(col2));
   }
 }
